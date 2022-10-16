e744bf5934f5df99bf98dbf6ee7a22455944c157
Mahdi Samavi <samavi.mahdi@gmail.com>
2022-10-16 22:40:28 +0330

    Title for site modified and deleted welcome page

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -10,7 +10,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title} / ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
