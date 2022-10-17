57b7d0da8c95a21c7a9efa51cf4b6f76bde9c757
Mahdi Samavi <samavi.mahdi@gmail.com>
2022-10-17 22:29:21 +0330

    Base Signin modal created

---
+++ resources/js/Modals/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Head, Link } from '@inertiajs/inertia-react';
+import Modal from '@/Components/Modal';
+
+export default function SignIn(props) {
+    return (
+        <>
+            <Head title="Log in to Twitter" />
+
+            <Modal></Modal>
+        </>
+    );
+}
