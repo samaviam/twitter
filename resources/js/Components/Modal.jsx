3a66827d3c32663ca11e0a6d565690ffee5ca9b8
Mahdi Samavi <samavi.mahdi@gmail.com>
2022-10-30 20:44:30 +0330

    Modal component modified

--- resources/js/Components/Modal.jsx
+++ resources/js/Components/Modal.jsx
@@ -6,27 +6,21 @@ export default function Modal(props) {
     const close = () => props.exitTo ? Inertia.visit(props.exitTo) : window.history.back();
 
     return (
-        <div tabIndex="-1" className="fixed flex justify-center items-center bg-[#5b708366] overflow-x-hidden top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
-            <div className="relative p-4 w-full max-w-[600px] h-full z-10 md:h-auto">
+        <div tabIndex="-1" className="fixed flex justify-center items-center bg-[#5b708366] overflow-x-hidden top-0 right-0 left-0 z-50 w-full h-screen md:inset-0 h-modal">
+            <div className="relative w-full max-w-[600px] h-full z-10 md:h-auto md:p-4">
                 {/* Modal content */}
-                <div className="relative bg-white rounded-lg shadow dark:bg-black">
+                <div className="relative h-full bg-white shadow dark:bg-black md:rounded-lg">
                     {/* Modal header */}
-                    <div className="flex justify-between items-center p-5 rounded-t border-b dark:border-gray-600">
-                        <h3 className="text-xl font-medium text-gray-900 dark:text-white">{props.title}</h3>
-                        <button type="button" onClick={close} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                    <div className="flex items-center p-2 rounded-t">
+                        <button type="button" onClick={close} className="w-9 h-9 justify-center items-center bg-transparent rounded-full text-sm inline-flex text-gray-400 hover:bg-gray-50 hover:bg-opacity-10 hover:text-gray-900 dark:hover:bg-gray-100 dark:hover:bg-opacity-10 dark:hover:text-white">
                             <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                            <span className="sr-only">Close modal</span> 
+                            <span className="sr-only">Close modal</span>
                         </button>
+                        <h2 className="ml-6 text-base font-twitterM font-medium text-gray-900 dark:text-white md:ml-8 md:text-xl">{props.title}</h2>
                     </div>
+
                     {/* Modal body */}
-                    <div className="p-6 space-y-6">
-                        {props.children}
-                    </div>
-                    {/* Modal footer */}
-                    <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
-                        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                        <button type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
-                    </div>
+                    {props.children}
                 </div>
             </div>
         </div>
